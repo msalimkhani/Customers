78b45fd17868398d8d98f1776e6dc809dcf1eed1
Mahdi salimkhani <m.salimkhani@iau.ir>
2024-05-05 18:18:05 +0330

    update

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/Linux/Customers
+# Install script for directory: D:/Linux/Customers/Customers
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/Linux/Customers/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/Linux/Customers/Customers/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
